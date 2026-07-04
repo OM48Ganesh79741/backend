f924552eb299adadfda02e87e346ede88447561d
Ganesh <ganeshpagar2895@gmail.com>
2026-07-04 22:52:09 +0530

    Remove Project2 from repository

--- roleAccessed/src/main/java/com/qsp/roleAccessed/repo/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qsp.roleAccessed.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qsp.roleAccessed.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
